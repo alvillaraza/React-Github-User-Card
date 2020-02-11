b8e073cf44b8052e1fbac29d0d4aab73f086d29a
alexis villaraza <alexis.borja@gmail.com>
2020-02-11 16:32:17 -0700

    added card styling to Followers

--- my-app/src/Followers.js
+++ my-app/src/Followers.js
@@ -1,19 +1,28 @@
 import React from 'react';
-
+import {
+    Card, CardImg, CardBody,
+    CardTitle, CardSubtitle, Row, Col
+  } from 'reactstrap';
 
 function Followers(props) {
     return (
-        <div>
+        <Row>
+        <Col xs="12" md="6" xl="3">
+            <Card>
             {props.followers.map(follower => (
+                <CardBody>
                 <div key={follower.id}>
-                <img src={follower.avatar_url} alt={`profile of ${follower.name}`} />
-                <h3>{follower.login}</h3>
-                <p>{follower.html_url}</p>
-                <p>{follower.bio}</p>
+                <CardImg top width='100%' src={follower.avatar_url} alt={`profile of ${follower.name}`} />
+                <CardTitle>{follower.login}</CardTitle>
+                <CardSubtitle>{follower.html_url}</CardSubtitle>
                 </div>
+              </CardBody>
             ))}
-        </div>
-    )
+            </Card>
+      
+        </Col>
+      </Row>
+    );
 }
 
 export default Followers;
